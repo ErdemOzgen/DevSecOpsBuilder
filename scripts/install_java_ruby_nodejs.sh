1b6e8e00901e61919e9e8e58a3a0be207793aa0b
ErdemOzgen <m.erdemozgen@gmail.com>
2023-11-29 18:57:51 +0300

    Update Node.js installation script

--- scripts/install_java_ruby_nodejs.sh
+++ scripts/install_java_ruby_nodejs.sh
@@ -14,10 +14,16 @@ if ! command -v node > /dev/null 2>&1
 then
     echo "Setting up Node.js..."
     ## Replace '20' with the version number you wish to install, e.g., '14' for Node.js 14
-    curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+    ##curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
     sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    NODE_MAJOR=20
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt-get update
+    source ~/.bashrc
     sudo apt install -y nodejs
-    sudo apt install -y npm
 else
     echo "Node.js is already installed."
 fi
