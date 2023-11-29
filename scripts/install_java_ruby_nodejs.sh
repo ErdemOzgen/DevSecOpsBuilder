93cb36c263e7c2c55007f1ae8922ce9b292428cf
ErdemOzgen <m.erdemozgen@gmail.com>
2023-11-29 18:51:19 +0300

    Update Node.js installation script

--- scripts/install_java_ruby_nodejs.sh
+++ scripts/install_java_ruby_nodejs.sh
@@ -14,17 +14,7 @@ if ! command -v node > /dev/null 2>&1
 then
     echo "Setting up Node.js..."
     ## Replace '20' with the version number you wish to install, e.g., '14' for Node.js 14
-    ##curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-    #curl -SLO https://deb.nodesource.com/nsolid_setup_deb.sh
-    #chmod 500 nsolid_setup_deb.sh
-    #sudo ./nsolid_setup_deb.sh 20
-    #source ~/.bashrc
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl gnupg
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-    NODE_MAJOR=20
-    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
     sudo apt-get update
     sudo apt install -y nodejs
     sudo apt install -y npm
